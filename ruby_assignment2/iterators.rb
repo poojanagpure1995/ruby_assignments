0b000fc8934228d1c6ade06843313eed8cbe7a0c
Pooja Nagpure <pooja.nagpure@wgblindia.com>
2018-05-08 20:56:00 +0530

    started ruby assignment 2

---
+++ ruby_assignment2/iterators.rb
@@ -0,0 +1,8 @@
+array_1 = [2, 4, 6, 8, 10]
+array_2 = [1, 5, 6, 8, 11, 12]
+
+hash_1 = {a: 'a', b: 'b', c: 'c', d: 'd', e: 'e'}
+hash_2 = {x: '10', y: '20', z: '30'}
+10.times { puts "Hello world" }
+puts  ('numbers between 30 and 40')
+(30...40) each {}
